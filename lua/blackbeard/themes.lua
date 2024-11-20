662741fc3c7e172c50a86b8eb9fe0f37bd1a21e9
cvusmo <blacksheepcosmo@gmail.com>
2024-11-20 15:36:08 -0600

    Simplifying lua

--- lua/blackbeard/themes.lua
+++ lua/blackbeard/themes.lua
@@ -25,26 +25,136 @@ end
 -- Light Theme
 M.light = function(palette)
     return {
+        -- Basic UI Elements
         Normal = {
             fg = palette.brblack,
             bg = palette.fg,
         },
-        CursorLine {
-          fg = palette.brblack,
+        CursorLine = {
+            bg = palette.blue,
         },
-        LineNr {
-          fg = palette.red,
+        LineNr = {
+            fg = palette.red,
         },
-        
+        CursorLineNr = {
+            fg = palette.black,
+            bold = true,
+        },
+        StatusLine = {
+            fg = palette.black,
+            bg = palette.fg,
+        },
+        StatusLineNC = {
+            fg = palette.brblack,
+            bg = palette.fg,
+        },
+        VertSplit = {
+            fg = palette.brblack,
+        },
+        Pmenu = {
+            fg = palette.black,
+            bg = palette.brblack,
+        },
+        PmenuSel = {
+            fg = palette.bg,
+            bg = palette.black,
+        },
+        Visual = {
+            bg = palette.blue,
+        },
+
+        -- Syntax Highlighting
         Comment = {
-            fg = palette.brblack,,
+            fg = palette.magenta,
             italic = true,
         },
         Constant = {
             fg = palette.black,
         },
+        String = {
+            fg = palette.red,
+        },
+        Character = {
+            fg = palette.black,
+        },
+        Number = {
+            fg = palette.red,
+        },
+        Boolean = {
+            fg = palette.blue,
+            bold = true,
+        },
         Identifier = {
-            fg = palette.selection_bg,
+            fg = palette.blue,
+        },
+        Function = {
+            fg = palette.red,
+            bold = true,
+        },
+        Statement = {
+            fg = palette.magenta,
+            bold = true,
+        },
+        Conditional = {
+            fg = palette.magenta,
+        },
+        Repeat = {
+            fg = palette.magenta,
+        },
+        Label = {
+            fg = palette.red,
+        },
+        Operator = {
+            fg = palette.black,
+        },
+        Keyword = {
+            fg = palette.red,
+            bold = true,
+        },
+        Exception = {
+            fg = palette.red,
+        },
+
+        -- NeoTree and File Explorer
+        NeoTreeDirectoryName = {
+            fg = palette.black,
+            bold = true,
+        },
+        NeoTreeDirectoryIcon = {
+            fg = palette.blue,
+        },
+        NeoTreeFileName = {
+            fg = palette.brblack,
+        },
+        NeoTreeFileNameOpened = {
+            fg = palette.red,
+            bold = true,
+        },
+
+        -- Diagnostics
+        DiagnosticError = {
+            fg = palette.red,
+        },
+        DiagnosticWarn = {
+            fg = palette.magenta,
+        },
+        DiagnosticInfo = {
+            fg = palette.blue,
+        },
+        DiagnosticHint = {
+            fg = palette.black,
+        },
+
+        -- Other UI Elements
+        SignColumn = {
+            fg = palette.brblack,
+        },
+        CursorColumn = {
+            bg = palette.bg,
+        },
+        Folded = {
+            fg = palette.brblack,
+            bg = palette.fg,
         },
     }
 end
